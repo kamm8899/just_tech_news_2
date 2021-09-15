58b270af8c3e6f3cbf6b7ba5fb51216d456d7349
kamm8899 <kamm8899@gmail.com>
2021-09-14 20:25:21 -0700

    debugged code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,41 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const {Post, User, Comment} = require('../models');
-const posts = dbPostData.map(post => post.get({ plain: true}));
+const { Post, User, Comment, Vote } = require('../models');
 
-router.get('/', (req, res) =>{
-    Post.findAll({
-        attrubutes:[
-            'id', 
-            'post_url', 
-            'title', 
-            'created_at', 
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment, 
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include:{
-                    model: User, 
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User, 
-                attributes: ['username']
-            }
-        ]
+// get all posts for homepage
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', { posts });
     })
-    .then(dbPostData =>{
-        console.log(dbPostData[0]);
-        //pass a single post object into the homepage template
-        res.render('homepage', {posts});
-    })
-    .catch(err =>{
-        console.log(err);
-        res.status(500).json(err);
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
